88d7e8b2c2fe07590b4b1627cfd4a2d5806d8f4b
William Brown <wbr38@uclive.ac.nz>
2024-10-12 00:37:32 +1300

    Add missing functions to grid.h

--- grid.h
+++ grid.h
@@ -12,6 +12,8 @@ typedef struct {
     bool tiles[TINYGL_WIDTH][TINYGL_HEIGHT];
 } grid_t;
 
-void grid_init();
+void grid_init(void);
+grid_t* grid_get(void);
+void grid_place_block(grid_t* grid, block_t* block);
 bool grid_valid_position(grid_t* grid, block_t* block, uint8_t x, uint8_t y, direction_t direction);
 #endif // GRID_H
